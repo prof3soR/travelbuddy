19e1acafe8cacd374f83b9b123180cfd513a2373
Rohit kumar <76482418+prof3soR@users.noreply.github.com>
2023-03-04 12:00:29 +0530

    Delete tavelbuddy directory

--- tavelbuddy/user_interface/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class profile(models.Model):
-    username=models.CharField(max_length=30, null=False)
-    mobile=models.CharField(max_length=10)
-    password=models.CharField(max_length=16)
